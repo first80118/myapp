376cd7ee5b0d3b3f5995816f92e754e6256a21a5
Ting <first971337@gmail.com>
2018-01-19 09:20:27 +0800

    Initial commit

---
+++ app/src/main/java/com/first80118/myapp/LotteryActivity.java
@@ -0,0 +1,13 @@
+package com.first80118.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LotteryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottery);
+    }
+}
